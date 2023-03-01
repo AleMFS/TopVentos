a92e953de1f5a4f1b41da5458886046152f621d4
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-03-01 17:50:40 -0300

    build

--- src/Screen/ProductSelected/index.tsx
+++ src/Screen/ProductSelected/index.tsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom"
 import { Ventiladores } from '../../../Ventiladores'
 import { BannerLogo, Carossel, Info, InfoProduct, Product, SelectedContainer } from "./styles";
-import Arno from "../../assets/Logos/Arno.png"
-import Britania from "../../assets/Logos/Britania.png"
-import Mallory from "../../assets/Logos/Mallory.png"
+import Arno from "../../assets/images/Logos/Arno.png"
+import Britania from "../../assets/images/Logos/Britania.png"
+import Mallory from "../../assets/images/Logos/Mallory.png"
 import { MutableRefObject, useState } from "react";
 import { Description } from "./components/Description";
 import {
