6c29655faf6a3373a3cbc0d1942d745676552106
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-03-01 19:09:17 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import copy from 'rollup-plugin-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        copy({
-          targets: [
-            { 
-              // copia os arquivos da pasta "src/assets/images" para a pasta "img" na pasta de saída
-              src: 'src/assets/images/*',
-              dest: 'images',
-            },
-          ],
-        }),
-      ],
-    },
-  },
-});
+})
