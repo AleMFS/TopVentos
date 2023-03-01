566b21ada555b40831b7711daddc149ad998a269
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-03-01 17:36:47 -0300

    update

--- src/Screen/ProductSelected/index.tsx
+++ src/Screen/ProductSelected/index.tsx
@@ -4,12 +4,52 @@ import { BannerLogo, Carossel, Info, InfoProduct, Product, SelectedContainer } f
 import Arno from "../../assets/Logos/Arno.png"
 import Britania from "../../assets/Logos/Britania.png"
 import Mallory from "../../assets/Logos/Mallory.png"
-import { useState } from "react";
+import { MutableRefObject, useState } from "react";
 import { Description } from "./components/Description";
-import { useKeenSlider } from "keen-slider/react"
+import {
+    useKeenSlider,
+    KeenSliderPlugin,
+    KeenSliderInstance,
+} from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
+import ScrollToTop from "./components/ScrollTop";
 
 
+
+function ThumbnailPlugin(
+    mainRef: MutableRefObject<KeenSliderInstance | null>
+): KeenSliderPlugin {
+    return (slider) => {
+        function removeActive() {
+            slider.slides.forEach((slide) => {
+                slide.classList.remove("active")
+            })
+        }
+        function addActive(idx: number) {
+            slider.slides[idx].classList.add("active")
+        }
+
+        function addClickEvents() {
+            slider.slides.forEach((slide, idx) => {
+                slide.addEventListener("click", () => {
+                    if (mainRef.current) mainRef.current.moveToIdx(idx)
+                })
+            })
+        }
+
+        slider.on("created", () => {
+            if (!mainRef.current) return
+            addActive(slider.track.details.rel)
+            addClickEvents()
+            mainRef.current.on("animationStarted", (main) => {
+                removeActive()
+                const next = main.animator.targetIdx || 0
+                addActive(main.track.absToRel(next))
+                slider.moveToIdx(Math.min(slider.track.details.maxIdx, next))
+            })
+        })
+    }
+}
 
 
 export function ProductSelected() {
@@ -18,32 +58,50 @@ export function ProductSelected() {
     const [image, setImage] = useState(0)
 
 
-    const [ref] = useKeenSlider<HTMLDivElement>({
-        slides: {
-          perView: 3.5,
-          spacing: 15,
+    const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+        initial: 0,
+    })
+    const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+        {
+            initial: 0,
+            slides: {
+                perView: 3.5,
+                spacing: 15,
+            },
         },
-      })
+        [ThumbnailPlugin(instanceRef)]
+    )
 
     return (
         <SelectedContainer>
+            <ScrollToTop/>
             <BannerLogo className={`${brand}`}>
                 <img src={brand === 'Arno' ? Arno : brand === 'Mallory' ? Mallory : Britania} alt="Logo" />
             </BannerLogo>
 
             <InfoProduct>
                 <Product>
-                    <img src={selectedProduct?.imagens[image]} alt="" />
+                    <div ref={sliderRef} className="keen-slider" >
+                        {selectedProduct?.imagens.map((imagens, index) => {
+                            return (
 
-                    <Carossel ref={ref} className="keen-slider" >
+                                <div className="keen-slider__slide">
+                                    <img src={imagens} alt="" />
+                                </div>
+
+                            )
+                        })}
+                    </div>
+
+                    <Carossel ref={thumbnailRef} className="keen-slider thumbnail" >
 
 
 
                         {selectedProduct?.imagens.map((imagens, index) => {
                             return (
-                                <div className="item keen-slider__slide" key={index}>
+                                <div className="keen-slider__slide item">
                                     <button onClick={() => setImage(index)}>
-                                        <img src={imagens} alt="" />    
+                                        <img src={imagens} alt="" />
                                     </button>
                                 </div>
                             )
